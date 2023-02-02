fa2a198d75ccbe131663952ba01f99b53b163232
hasan <hasanrizwan6@gmail.com>
2023-02-03 03:22:21 +0500

    code to redirect to home in case of unknown route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { Home } from './components/Home';
 import { Launch } from './components/Launch';
 import { Navbar } from './components/Navbar';
 import { LaunchIndex } from './components/LaunchIndex';
-import { NotFound } from './components/NotFound';
 import { LaunchShoe } from './components/LaunchShoe';
 
 function App() {
@@ -19,14 +18,13 @@ function App() {
         <Navbar />
       </div>
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path='launch' element={<Launch />}>
           <Route path='' element={<LaunchIndex />} />
           <Route path=':slug' element={<LaunchShoe />} />
           {/* <Route path='air-jordan-3-valor-blue' element={<LaunchShoe />} />
           <Route path='jordan-mars-270-london' element={<LaunchShoe />} /> */}
         </Route>
-        <Route path='*' element={<NotFound />} />
+        <Route path='*' element={<Home />} />
       </Routes>
     </Router>
   );
